f7fe717f644074519323c30ece564910a2393cf5
Eric Emery <ericemery099@gmail.com>
2023-08-17 13:34:39 +0000

    updated about us file

--- my-app/src/app/About.js
+++ my-app/src/app/About.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div classname="container-fluid">
+      <div classname="col">
+      <div classname="col">
+        <h4>Why Stat Track?</h4>
+      </div>
+      <div classname="row">
+        <div classname="col">
+          <p>With a tracker, you can get up-to-date reports of your performance and post-game reports that will show you how you've done. More importantly, you can learn your average points in the field you care about, which will help you figure out what you need to focus on.</p>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
