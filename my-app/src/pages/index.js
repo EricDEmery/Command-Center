d074aa67ee81da63e9674a63c77d72a045d663f3
Eric Emery <ericemery099@gmail.com>
2023-08-22 04:48:32 +0000

    added API call

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,6 +1,28 @@
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://rocket-league1.p.rapidapi.com/ranks/KujohLive/',
+  
+  headers: {
+    'User-Agent': 'RapidAPI Playground',
+    'Accept-Encoding': 'identity',
+    'X-RapidAPI-Key': '5fc0357622msh5ccf58c452c805dp156dafjsnf40ae832c05a',
+    'X-RapidAPI-Host': 'rocket-league1.p.rapidapi.com',
+    'x-rapidapi-ua': 'RapidAPI-Playground'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
+
 export default function Page() {
     return <div>
       <h1>Home page JDH</h1>
