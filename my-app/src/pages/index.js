51a2ed7fc3a48686f2f9be919608b5b3517faa00
Eric Emery <ericemery099@gmail.com>
2023-08-23 06:45:35 +0000

    added search feature

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,24 +1,30 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import axios from 'axios'
+import axios from "axios";
 
-const api = "https://8000-ericdemery-commandcente-0d2at847xvr.ws-us104.gitpod.io/app/search/";
+const api =
+  "https://8000-ericdemery-commandcente-0d2at847xvr.ws-us104.gitpod.io/app/search/";
 
 export default function SearchPage() {
-  const [username, setUsername] = useState('');
   const [ranks, setRanks] = useState([]);
 
-  async function fetchData() {
+  function handleInput(e) {
+    if (e.key === "Enter") {
+      fetchData(e.target.value);
+    }
+  }
+
+  async function fetchData(username) {
     try {
       const response = await axios.get(`${api}?username=${username}`);
       setRanks(response.data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   }
-console.log(username)
-console.log(ranks)
+
+  console.log(ranks);
   return (
     <>
       <h1>Rocket League Stat Tracker</h1>
@@ -29,13 +35,9 @@ console.log(ranks)
 
       <input
         type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
         placeholder="Enter Epic ID"
+        onKeyDown={(e) => handleInput(e)}
       />
-      <button onClick={fetchData}>Search</button>
-      
-
     </>
   );
 }
